d038f1b99edcd1b57ebc85fc68e94956c4c80712
LuzhiZ <luzhizhaowen@foxmail.com>
2018-01-18 15:53:00 +0800

    add groupchat processing function

--- collectData.py
+++ collectData.py
@@ -5,6 +5,7 @@ import datetime
 import sys
 import base64 as b
 from User import User
+from itchat.content import *
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -13,9 +14,10 @@ mainObject = User()
 
 
 #sql
-sel_user_by_nick= 'select * from luzhibot_user where nickName64 = %s'
-sel_friend_by_info='select * from luzhibot_friend where nickName64 = %s and remarkName = %s and sex = %s and centralId = %s'
+sel_user_by_nick= 'select * from luzhibot_user where nickName = %s'
+sel_friend_by_info='select * from luzhibot_friend where nickName = %s and remarkName = %s and sex = %s and centralId = %s'
 sel_chatroom_by_info='select * from luzhibot_chatroom where chatRoomNickName = %s and personNum = %s and centralId = %s'
+sel_dailog_by_info='select * from  luzhibot_dailog where msgPath=%s and msgType=%s and msg=%s and senderNick=%s and receiverNick=%s and time=%s'
 
 ins_user='insert into luzhibot_user(userName,passWord,weixinId,nickName,remarkName,headImage,signature,sex) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
 ins_friends='insert into luzhibot_friend(userName,weixinId,nickName,remarkName,headImage,sex,centralId) VALUES (%s,%s,%s,%s,%s,%s,%s)'
@@ -27,26 +29,30 @@ conn = MySQLdb.connect(
         user = 'root',
         passwd = '123456',
         db = 'wechat',
-        charset = 'utf8mb4',
+        charset='utf8mb4'
 )
 if conn :
     cur = conn.cursor()
+    cur.execute("set names utf8mb4")
+    cur.execute("SET CHARACTER SET utf8mb4")
+    cur.execute("SET character_set_connection=utf8mb4")
+    conn.commit()
 else :
     exit()
 
 #itchat  login and personly text messages
-@it.msg_register(it.content.TEXT)
+@it.msg_register(TEXT)
 def process_msg(msg):
     if msg.text=='login' and not mainObject.get_loginstatus():#login
         user=it.search_friends(userName=msg.FromUserName)
         if user :
             try :
-                count=cur.execute(sel_user_by_nick, [b.b64encode(user.nickName)])
+                count=cur.execute(sel_user_by_nick, [user.nickName])
                 if count == 1:
                     it.send_msg('login successfully',toUserName='filehelper')
 
                 elif count ==0:
-                    cur.execute(ins_user , [user.userName,'','',b.b64encode(user.nickName),user.remarkName,user.HeadImgUrl,user.signature,user.sex])#register new user
+                    cur.execute(ins_user , [user.userName,'','',user.nickName,user.remarkName,user.HeadImgUrl,user.signature,user.sex])#register new user
                     conn.commit()
                     it.send_msg('register and first logged in  successfully ', toUserName='filehelper')
                 else:
@@ -55,7 +61,7 @@ def process_msg(msg):
                 print(Exception)
                 it.send_msg('login failed','filehelper')
 
-            count = cur.execute(sel_user_by_nick, [b.b64encode(user.nickName)])
+            count = cur.execute(sel_user_by_nick, [user.nickName])
             centralId = cur.fetchone()[0]
 
             #导入所有friend信息
@@ -65,9 +71,9 @@ def process_msg(msg):
 
             friends ,chatrooms =[],[]
             for friend in friendsList:
-                count = cur.execute(sel_friend_by_info,(b.b64encode(friend.nickName),friend.remarkName,friend.sex,str(centralId)))
+                count = cur.execute(sel_friend_by_info,(friend.nickName,friend.remarkName,friend.sex,str(centralId)))
                 if count ==0:
-                    friends.append((friend.userName,'',b.b64encode(friend.nickName),friend.remarkName,friend.HeadImgUrl,str(friend.sex),str(centralId)))
+                    friends.append((friend.userName,'',friend.nickName,friend.remarkName,friend.HeadImgUrl,str(friend.sex),str(centralId)))
                 elif count ==1:
                     continue
             if len(friends)>0:
@@ -79,11 +85,11 @@ def process_msg(msg):
             # sel_chatroom_by_info='select * from luzhibot_chatroom where chatRoomNickName = %s and personNum = %s and centralId = %s and ownerId = %s
             # 'ins_chatroom='insert into luzhibot_chatroom(chatRoomUserName,personNum,chatRoomNickName,centralId,ownerUserName) VALUES (%s,%s,%s,%s,%s)'
             for chatroom in chatroomsList:
-                count = cur.execute(sel_chatroom_by_info,(b.b64encode(chatroom.NickName),chatroom.MemberCount,str(centralId)))
+                count = cur.execute(sel_chatroom_by_info,(chatroom.NickName,chatroom.MemberCount,str(centralId)))
                 if count == 0:
                     if not hasattr(chatroom,'ChatRoomOwner'):
                         setattr(chatroom,'ChatRoomOwner','')
-                    chatrooms.append((chatroom.UserName,chatroom.MemberCount,b.b64encode(chatroom.NickName),str(centralId),chatroom.ChatRoomOwner))
+                    chatrooms.append((chatroom.UserName,chatroom.MemberCount,chatroom.NickName,str(centralId),chatroom.ChatRoomOwner))
                 elif count == 1:
                     continue
             if len(chatrooms)>0:
@@ -104,15 +110,18 @@ def process_msg(msg):
         sender=it.search_friends(userName=msg.FromUserName)
         receiver=it.search_friends(userName=msg.ToUserName)
         dt = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        cur.execute(ins_dailog,('','Text',msg.text,b.b64encode(sender.NickName),b.b64encode(receiver.NickName),dt))
+        cur.execute(ins_dailog,('','Text',msg.text,sender.NickName,receiver.NickName,dt))
         conn.commit()
-
+        #验证emoji
+        # count=cur.execute(sel_dailog_by_info,['','Text',msg.text,sender.NickName,receiver.NickName,dt])
+        # if count ==1:
+        #     it.send_msg(cur.fetchone()[3],'filehelper')
 
     else:
         print (msg.text)
 
 #itchat grouply text message
-@it.msg_register(it.content.TEXT,isGroupChat=True)
+@it.msg_register(TEXT,isGroupChat=True)
 def process_group(msg):
     actualNickName=it.search_friends(userName=msg.ActualUserName).NickName
     if mainObject.get_loginstatus():
@@ -124,8 +133,115 @@ def process_group(msg):
             receiver = it.search_chatrooms(userName=msg.FromUserName)
 
         dt = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        cur.execute(ins_dailog,('','GroupText',msg.text,b.b64encode(sender.NickName),b.b64encode(receiver.NickName),dt))
+        cur.execute(ins_dailog,('','GroupText',msg.text,sender.NickName,receiver.NickName,dt))
         conn.commit()
+
+#itchat  personly other type text message
+@it.msg_register([SHARING,MAP])
+def process_sm(msg):
+    if mainObject.get_loginstatus():
+        sender=it.search_friends(userName=msg.FromUserName)
+        receiver=it.search_friends(userName=msg.ToUserName)
+        dt = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        cur.execute(ins_dailog,(msg.Url,msg.Type,msg.text,sender.NickName,receiver.NickName,dt))
+        conn.commit()
+        it.send_msg('%s:%s:%s'%(msg.Type,msg.text,msg.Url),'filehelper')
+
+@it.msg_register(CARD)
+def process_card(msg):
+    if mainObject.get_loginstatus():
+        sender=it.search_friends(userName=msg.FromUserName)
+        receiver=it.search_friends(userName=msg.ToUserName)
+        dt = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        cur.execute(ins_dailog,('',msg.Type,msg.text.NickName,sender.NickName,receiver.NickName,dt))
+        conn.commit()
+        it.send_msg('%s:%s'%(msg.Type,msg.Text.NickName),'filehelper')
+
+@it.msg_register(NOTE)
+def process_note(msg):
+    if mainObject.get_loginstatus():
+        sender=it.search_friends(userName=msg.FromUserName)
+        receiver=it.search_friends(userName=msg.ToUserName)
+        dt = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        cur.execute(ins_dailog,('',msg.Type,msg.Text,sender.NickName,receiver.NickName,dt))
+        conn.commit()
+        it.send_msg('%s:%s'%(msg.Type,msg.text),'filehelper')
+
+@it.msg_register(ATTACHMENT)
+def process_file(msg):
+    if mainObject.get_loginstatus():
+        sender=it.search_friends(userName=msg.FromUserName)
+        receiver=it.search_friends(userName=msg.ToUserName)
+        dt = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        cur.execute(ins_dailog,('',msg.Type,msg.FileName,sender.NickName,receiver.NickName,dt))
+        conn.commit()
+        it.send_msg('%s:%s'%(msg.Type,msg.text),'filehelper')
+
+
+#itchat  grouply other type text message
+@it.msg_register([SHARING,MAP],isGroupChat=True)
+def group_process_sm(msg):
+    if mainObject.get_loginstatus():
+        actualNickName = it.search_friends(userName=msg.ActualUserName).NickName
+        if mainObject.get_loginstatus():
+            if b.b64encode(mainObject.get_nickname()) == b.b64encode(actualNickName):
+                sender = it.search_friends(userName=msg.FromUserName)
+                receiver = it.search_chatrooms(userName=msg.ToUserName)
+            else:
+                sender = it.search_friends(userName=msg.ActualUserName)
+                receiver = it.search_chatrooms(userName=msg.FromUserName)
+
+        dt = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        cur.execute(ins_dailog,(msg.Url,'Group'+msg.Type,msg.text,sender.NickName,receiver.NickName,dt))
+        conn.commit()
+        it.send_msg('%s:%s:%s'%('Group'+msg.Type,msg.text,msg.Url),'filehelper')
+
+@it.msg_register(CARD,isGroupChat=True)
+def group_process_card(msg):
+    if mainObject.get_loginstatus():
+        actualNickName = it.search_friends(userName=msg.ActualUserName).NickName
+        if mainObject.get_loginstatus():
+            if b.b64encode(mainObject.get_nickname()) == b.b64encode(actualNickName):
+                sender = it.search_friends(userName=msg.FromUserName)
+                receiver = it.search_chatrooms(userName=msg.ToUserName)
+            else:
+                sender = it.search_friends(userName=msg.ActualUserName)
+                receiver = it.search_chatrooms(userName=msg.FromUserName)
+
+        dt = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        cur.execute(ins_dailog,('','Group'+msg.Type,msg.text['NickName'],sender.NickName,receiver.NickName,dt))
+        conn.commit()
+        it.send_msg('%s:%s'%('Group'+msg.Type,msg.text['NickName']),'filehelper')
+
+@it.msg_register(NOTE,isGroupChat=True)# 通知信息由群发出 ，由user接收
+def group_process_note(msg):
+    if mainObject.get_loginstatus():
+        sender = it.search_chatrooms(userName=msg.FromUserName)
+        receiver = it.search_friends(userName=msg.ToUserName)
+
+        dt = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        cur.execute(ins_dailog,('','Group'+msg.Type,msg.Text,sender.NickName,receiver.NickName,dt))
+        conn.commit()
+        it.send_msg('%s:%s'%('Group'+msg.Type,msg.text),'filehelper')
+
+@it.msg_register(ATTACHMENT,isGroupChat=True)
+def group_process_file(msg):
+    if mainObject.get_loginstatus():
+        actualNickName = it.search_friends(userName=msg.ActualUserName).NickName
+        if mainObject.get_loginstatus():
+            if b.b64encode(mainObject.get_nickname()) == b.b64encode(actualNickName):
+                sender = it.search_friends(userName=msg.FromUserName)
+                receiver = it.search_chatrooms(userName=msg.ToUserName)
+            else:
+                sender = it.search_friends(userName=msg.ActualUserName)
+                receiver = it.search_chatrooms(userName=msg.FromUserName)
+
+        dt = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        cur.execute(ins_dailog,('','Group'+msg.Type,msg.FileName,sender.NickName,receiver.NickName,dt))
+        conn.commit()
+        it.send_msg('%s:%s'%('Group'+msg.Type,msg.FileName),'filehelper')
+
+
 
 def loginlookup():
     it.send_msg('please send login to filehelper',toUserName='filehelper')
--- try.py
+++ try.py
@@ -1,16 +1,45 @@
 #encoding:utf-8
 import itchat as it
+from  itchat.content import *
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
+# TEXT       = 'Text'
+# MAP        = 'Map'
+# CARD       = 'Card'
+# NOTE       = 'Note'
+# SHARING    = 'Sharing'
+# PICTURE    = 'Picture'
+# RECORDING  = VOICE = 'Recording'
+# ATTACHMENT = 'Attachment'
+# VIDEO      = 'Video'
+# FRIENDS    = 'Friends'
+# SYSTEM     = 'System'
 
-@it.msg_register(it.content.TEXT)
-def process(msg):
-    print(msg.text)
+# @it.msg_register(TEXT, isGroupChat=True)
+# def group_msg(msg):
+#     print(msg)
 
-@it.msg_register(it.content.TEXT, isGroupChat=True)
-def group_msg(msg):
-    print(msg)
+# @it.msg_register(NOTE)
+# def process_note(msg):
+#     print(msg)
+
+@it.msg_register([MAP, CARD, NOTE, SHARING])
+def text_reply(msg):
+    msg.user.send('%s: %s' % (msg.type, msg.text))
+
+@it.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO])
+def download_files(msg):
+    msg.download(msg.fileName)
+    typeSymbol = {
+        PICTURE: 'img',
+        VIDEO: 'vid', }.get(msg.type, 'fil')
+    return '@%s@%s' % (typeSymbol, msg.fileName)
+
+@it.msg_register(FRIENDS)
+def add_friend(msg):
+    msg.user.verify()
+    msg.user.send('Nice to meet you!')
 it.auto_login(hotReload=True)
 friends=it.search_friends()
 chatrooms =it.search_chatrooms()
